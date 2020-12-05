675f3f7862222f93ef054ea312bbf8b5b6aae1c7
gluttonythesin <75030164+gluttonythesin@users.noreply.github.com>
2020-12-05 12:22:39 +0100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -33,17 +33,17 @@ the things I can help you with.
 If you have any questions on how to use me, head over to @PhoenixSupport
 
 *Main* commands available:
- - !start: start the bot
- - !help: PM's you this message.
- - !help <module name>: PM's you info about that module.
- - !donate: information about how to donate!
- - !settings:
+ - +start: start the bot
+ - +help: PM's you this message.
+ - +help <module name>: PM's you info about that module.
+ - +donate: information about how to donate!
+ - +settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
 
 {}
 And the following:
-""".format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+""".format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with + or !.\n")
 
 DONATE_STRING = """{0} doesn't currently need any donations.
 However you can donate to the creator of the original source code which {0} \
